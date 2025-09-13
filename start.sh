b1c005a78725ad9632967ba082c6e89d3509336a
Gustavo Atanazio <gus.atanazio@gmail.com>
2025-09-13 18:31:29 -0300

    chore: ajustando execução de migrations do Flyway

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 echo "--- Iniciando banco MySQL ---"
 docker-compose up -d mysql
 
+echo "--- Executando migrations Flyway ---"
+docker-compose run --rm flyway
+
 trap 'echo "--- Parando banco MySQL ---"; docker-compose down' EXIT
 
 echo "--- Iniciando aplicação Java ---"
